5dac0e73348b36a556bfe86776a9b34c56e9c919
vivek <vivek@athenaeum.in>
2021-03-15 17:08:51 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -3,37 +3,17 @@ package in.athenaeum;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        //  Anonymous class implementation
-        //  https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html
-//        Thread t = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//
-//            }
-//        });
+        Map<String, Double> population = Map.of("Mumbai", 1.8, "Bengaluru", 1.3, "Chennai", 1.0);
 
-
-        Thread t = new Thread(() -> System.out.println(Thread.currentThread().getName()));
-
-        t.start();
-
-        System.out.println(Thread.currentThread().getName());
-
-        List<String> names = new ArrayList<>();
-        names.add("Sunil Nagar");
-        names.add("Madhavanand");
-        names.add("Rahul");
-        names.add("Shrikar Joshi");
-
-        names.sort((s1, s2) -> s2.length() - s1.length());
-
-        for(String name: names) {
-            System.out.println(name);
+        for(Map.Entry<String, Double> entry :population.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
