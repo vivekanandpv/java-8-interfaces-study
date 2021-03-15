ea448913d76b9d58184db52da406c3e359d7c6a8
vivek <vivek@athenaeum.in>
2021-03-15 16:34:54 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,18 +1,28 @@
 package in.athenaeum;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.IntSupplier;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        foo((s) -> System.out.println(s));
+        //  Function<String, Integer> stringLengthFunction = s -> s.length();
+
+        foo(i -> System.out.println(i), s -> s.length());
+
+        BiFunction<String, LocalDateTime, String> concatenateBiFunction = (s1, ldt) -> s1 + " " + ldt.toString();
+
+        System.out.println(concatenateBiFunction.apply("Hello", LocalDateTime.now()));
+
+        Predicate<String> isAllCaps = s -> s.equals(s.toUpperCase());
+
+        System.out.println(isAllCaps.test("Fails"));
+        System.out.println(isAllCaps.test("PASSES"));
     }
 
-    public static void foo(Consumer<String> consumer) {
+    public static void foo(Consumer<Integer> consumer, Function<String, Integer> transformer) {
         List<String> names = new ArrayList<>();
         names.add("Sunil");
         names.add("Ankur");
@@ -20,7 +30,9 @@ public class Main {
         names.add("Madhu");
 
         for(String name: names) {
-            consumer.accept(name);
+            consumer.accept(transformer.apply(name));
         }
     }
+
+
 }
