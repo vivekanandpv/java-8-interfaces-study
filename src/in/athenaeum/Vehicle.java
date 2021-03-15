2559f88c97c4962081c36ad795519bc9fd4f0e97
vivek <vivek@athenaeum.in>
2021-03-15 15:26:22 +0530

    done

--- src/in/athenaeum/Vehicle.java
+++ src/in/athenaeum/Vehicle.java
@@ -1,8 +1,13 @@
 package in.athenaeum;
 
+//  As such this annotation is optional
+//  But, it is a good measure (just in case be break the rule)
+//  Also, it makes out intent clear
+@FunctionalInterface
 public interface Vehicle {
-    void drive();
+    void drive();   //  one and only one abstract method
 
+    //  there can be several default or static methods
     default void honk() {
         System.out.println("Default: honk...");
     };
