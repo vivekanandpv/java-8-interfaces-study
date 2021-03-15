2191de16ac70ab00352ecae399711d29e53c819d
vivek <vivek@athenaeum.in>
2021-03-15 16:53:37 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -8,31 +8,32 @@ import java.util.function.*;
 public class Main {
 
     public static void main(String[] args) {
-        //  Function<String, Integer> stringLengthFunction = s -> s.length();
+        //  Anonymous class implementation
+        //  https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
 
-        foo(i -> System.out.println(i), s -> s.length());
 
-        BiFunction<String, LocalDateTime, String> concatenateBiFunction = (s1, ldt) -> s1 + " " + ldt.toString();
+        Thread t = new Thread(() -> System.out.println(Thread.currentThread().getName()));
 
-        System.out.println(concatenateBiFunction.apply("Hello", LocalDateTime.now()));
+        t.start();
 
-        Predicate<String> isAllCaps = s -> s.equals(s.toUpperCase());
+        System.out.println(Thread.currentThread().getName());
 
-        System.out.println(isAllCaps.test("Fails"));
-        System.out.println(isAllCaps.test("PASSES"));
-    }
-
-    public static void foo(Consumer<Integer> consumer, Function<String, Integer> transformer) {
         List<String> names = new ArrayList<>();
-        names.add("Sunil");
-        names.add("Ankur");
-        names.add("Vighnesh");
-        names.add("Madhu");
+        names.add("Sunil Nagar");
+        names.add("Madhavanand");
+        names.add("Rahul");
+        names.add("Shrikar Joshi");
+
+        names.sort((s1, s2) -> s2.length() - s1.length());
 
         for(String name: names) {
-            consumer.accept(transformer.apply(name));
+            System.out.println(name);
         }
     }
-
-
 }
