ebd23e51a507ce145b174fe6b681b72259e5c24a
vivek <vivek@athenaeum.in>
2021-03-15 16:16:23 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -2,32 +2,25 @@ package in.athenaeum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
-        Supplier<String> stringSupplier = () -> {
-            //...
-            return "Hi there!";
-        };
-
-        //  shorter version with implicit return
-        //  Supplier<String> stringSupplier = () -> "Hi there!";
-
-        foo(stringSupplier);
+        foo((s) -> System.out.println(s));
     }
 
-    public static void foo(Supplier<String> supplier) {
+    public static void foo(Consumer<String> consumer) {
         List<String> names = new ArrayList<>();
         names.add("Sunil");
         names.add("Ankur");
         names.add("Vighnesh");
         names.add("Madhu");
 
-        for(String s: names) {
-            System.out.println(supplier.get() + " " + s);
+        for(String name: names) {
+            consumer.accept(name);
         }
     }
 }
